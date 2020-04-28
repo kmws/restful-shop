5403212db5ae9630655caa53c3d4a216573e75c0
luwso <lukasz.wsol@protonmail.com>
2020-04-28 09:14:47 +0200

    delete: dummy files

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,8 @@ from flask import Blueprint
 
 
 def register_routes(app, api):
-    from app.api.dummy_api import ns as dummy_namespace
-    api.add_namespace(dummy_namespace)
+    # from app.api.dummy_api import ns as dummy_namespace
+    # api.add_namespace(dummy_namespace)
 
     api_bp = Blueprint('api', __name__, url_prefix='/api')
     api.init_app(api_bp)
